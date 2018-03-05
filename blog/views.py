bd673fca52e5316664cd1129c534e9402ff7a53d
wofstech <gabrielabuka3@gmail.com>
2018-03-05 07:30:15 -0700

    'added email'

--- blog/views.py
+++ blog/views.py
@@ -28,7 +28,7 @@ def contact(request):
             from_email = form.cleaned_data['Your_email']
             message = form.cleaned_data['message']
             try:
-                send_mail(subject, message, from_email, ['admin@example.com'])
+                send_mail(subject, message, from_email, ['gabrielabuka3@gmail.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('success')
